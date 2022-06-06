36115e992be211683761c6e8db70dcd1400d8327
liaomin111 <51534280+liaomin111@users.noreply.github.com>
2022-06-06 11:49:02 +0800

    benchmarks

---
+++ benchmarks/NPB/FT/npbparams.h
@@ -0,0 +1,26 @@
+/* CLASS = W */
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   
+*/
+#define NX             8
+#define NY             8
+#define NZ             8
+#define MAXDIM         128
+#define NITER_DEFAULT  2
+#define NXP            129
+#define NYP            128
+#define NTOTAL         524288
+#define NTOTALP        528384
+
+#define CONVERTDOUBLE  false
+#define COMPILETIME "23 Sep 2021"
+#define NPBVERSION "3.3.1"
+#define CS1 "gcc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-g -Wall -O3 -mcmodel=small"
+#define CS6 "-O3 -mcmodel=small"
+#define CS7 "randdp"
---
+++ benchmarks/NPB/SP/npbparams.h
@@ -0,0 +1,20 @@
+/* CLASS = W */
+/*
+   This file is generated automatically by the setparams utility.
+   It sets the number of processors and the class of the NPB
+   in this directory. Do not modify it by hand.   
+*/
+#define PROBLEM_SIZE   3
+#define NITER_DEFAULT  3
+#define DT_DEFAULT     0.0015
+
+#define CONVERTDOUBLE  false
+#define COMPILETIME "23 Sep 2021"
+#define NPBVERSION "3.3.1"
+#define CS1 "gcc"
+#define CS2 "$(CC)"
+#define CS3 "-lm"
+#define CS4 "-I../common"
+#define CS5 "-g -Wall -O3 -mcmodel=small"
+#define CS6 "-O3 -mcmodel=small"
+#define CS7 "randdp"
