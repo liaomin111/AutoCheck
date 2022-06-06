1e7eaaca0add42b7167e8b200937b312f6b39f5f
liaomin111 <51534280+liaomin111@users.noreply.github.com>
2022-06-06 11:50:47 +0800

    CoMD

---
+++ benchmarks/CoMD/src-mpi/CoMD_info.h
@@ -0,0 +1,15 @@
+#ifndef CoMD_info_hpp
+#define CoMD_info_hpp
+
+#define CoMD_VARIANT "CoMD-serial"
+#define CoMD_HOSTNAME "1a1e493e9915"
+#define CoMD_KERNEL_NAME "'Linux'"
+#define CoMD_KERNEL_RELEASE "'5.4.0-109-generic'"
+#define CoMD_PROCESSOR "'x86_64'"
+
+#define CoMD_COMPILER "'/usr/bin/gcc'"
+#define CoMD_COMPILER_VERSION "'gcc (Ubuntu 5.4.0-6ubuntu1~16.04.6) 5.4.0 20160609'"
+#define CoMD_CFLAGS "'-std=c99'"
+#define CoMD_LDFLAGS "''"
+
+#endif
